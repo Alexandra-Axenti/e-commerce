e3393f1eaec1983d965f738b548afd10edb7f652
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-08 18:06:35 +0800

    working on the product-edit component

--- app/components/product-edit.js
+++ app/components/product-edit.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editProductForm: false,
+
+  actions: {
+    editProductForm() {
+      this.set('editProductForm', true);
+  },
+    update(product) {
+      var params = {
+        name: this.get('name'),
+        brand: this.get('brand'),
+        content: this.get('content'),
+        image: this.get('image'),
+        price: parseInt(this.get('price')),
+        stock: parseInt(this.get('stock'))
+      };
+      this.set('editProductForm', false);
+      this.sendAction('update', product, params);
+    }
+  }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,5 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('product');
-  }
+},
+
+    actions:{
+      update(product, params) {
+        Object.keys(params).forEach(function(key) {
+          if(params[key]!==undefined && params[key]!==NaN) {
+            product.set(key,params[key]);
+          }
+          product.save();
+        });
+        this.transitionTo('index', params);
+      },
+    }
 });
