20750296ee3be485089548aa9d810aa5039b267f
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-09 11:36:57 +0800

    added empty values to new form component new-product to avoid error

--- app/components/new-product.js
+++ app/components/new-product.js
@@ -10,12 +10,12 @@ export default Ember.Component.extend({
 
     saveProduct() {
       var params = {
-        name: this.get('name'),
-        brand: this.get('brand'),
-        content: this.get('content'),
-        image: this.get('image'),
-        price: this.get('price'),
-        stock: this.get('stock')
+        name: this.get('name') ? this.get('name') : "",
+        brand: this.get('brand') ? this.get('brand') : "",
+        content: this.get('content') ? this.get('content') : "",
+        image: this.get('image') ? this.get('image') : "",
+        price: this.get('price') ? this.get('price') : 0,
+        stock: this.get('stock') ? this.get('stock') : 0
       };
       this.set('newProductForm', false);
       this.sendAction('saveProduct', params);
